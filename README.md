fb821281df0fa8ef1d5f027b58d2216825c85b98
pico-sol <qmata55@gmail.com>
2024-10-23 01:29:32 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Solana Validator Shell Scripts
 
+active-to-inactive.sh: change ***.***.***.*** to IP address of your inactive node
+
 c and m are shortcut symlink for catchup and monitor like 'bash c' or 'bash m'
--- active-to-inactive.sh
+++ active-to-inactive.sh
@@ -5,4 +5,4 @@
 solana-validator -l /mnt/ledger wait-for-restart-window --min-idle-time 2 --skip-new-snapshot-check
 solana-validator -l /mnt/ledger set-identity /home/solv/unstaked-identity.json
 ln -sf /home/solv/unstaked-identity.json /home/solv/identity.json
-scp /mnt/ledger/tower-1_9-$(solana-keygen pubkey /home/solv/mainnet-validator-keypair.json).bin solv@107.155.109.146:/mnt/ledger
+scp /mnt/ledger/tower-1_9-$(solana-keygen pubkey /home/solv/mainnet-validator-keypair.json).bin solv@***.***.***.***:/mnt/ledger
